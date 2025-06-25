e034d91abd123182cedf2058f9670d1d38dfbe15
edufonto22 <eduardofontora7@gmail.com>
2025-06-25 20:08:49 -0300

    se modificó la base de datos

--- Base_de_Datos/fontora_ed.sql
+++ Base_de_Datos/fontora_ed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-06-2025 a las 03:42:29
+-- Tiempo de generación: 26-06-2025 a las 01:07:38
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -62,11 +62,13 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `usuario`, `email`, `pass`, `perfil_id`, `baja`) VALUES
-(1, 'Edu', 'Fontora', 'random', 'random@correo.com', '$2y$10$A42ZsrNcyAYdwDl4m4fM6ufqRbrYoindFisFOoZYohSkduezA7sti', 2, 'NO'),
-(2, 'Roberto', 'Gomez', 'elchavo', 'elchavo@correo.com', '$2y$10$WVZX1Du5x7t8aBqnKRTxEeQbHwy9/oy0x41Ixg.ZuoqOWl/rjBAB2', 2, 'NO'),
-(19, 'edu', 'fontora', 'edu123', 'edu@correo.com', '$2y$10$U8k4s9qOgDmmjkbdliJWoe24gMhXZz5qV7bpzJhCDFWD/4ORbUeWu', 2, 'NO'),
-(20, 'edu', 'juan', 'eduju123', 'eduarju@correo.com', '$2y$10$hdkERrYov8j6Ud6R6IE4fevM/QjHT4VpJD5.CvCAgD7YOYYS78ETq', 2, 'NO'),
-(21, 'raul', 'gomez', 'rau123', 'rau@correo.com', '$2y$10$hUtmRGLv7zZcnocMgquWxemFvufx0zRhlHQn43DVykqmca3ReYUsW', 2, 'NO');
+(1, 'Admin', 'Principal', 'admin', 'admin@admin.com', '$2y$10$tmDI1WMXflFsoHJk9XCnW./PlLqpWtOjyY8VNsKrBQ0kAXKFX6SZu', 1, 'NO'),
+(2, 'Edu', 'Fontora', 'edu22', 'eduar@correo.com', '$2y$10$b7HeG4oXtkv4xgZylSHMjOi8XSunSFEk1acule.iM32BYH/bSUZ5O', 0, ''),
+(4, 'Facundo', 'Gomez', 'facu12', 'facugo@correo.com', '$2y$10$2NeBGnqbJSkJfoJpxuLmmuQ8rqlKOfxJumRdYBut.wkPrL4GagPdu', 2, 'NO'),
+(5, 'Ramon', 'Valdez', 'ramonval12', 'ramon@correo.com', '$2y$10$DBCmvfZElCToN/5IwYZYEORkN7Ns2ZD1DFwZUTwLHrVvN.0APRM4y', 2, 'NO'),
+(7, 'Franco', 'Mastantuono', 'mastan123', 'francocarp@river.com', '$2y$10$7cN7gQvDpmsJ.l3bMcSg0eJP8hXI2vV3Gwqb3vw39f53jSTLH3qR6', 2, 'NO'),
+(8, 'Gonzalo', 'Montiel', 'gonzamonti', 'somostodosmontiel@river.com', '$2y$10$HjOhb1rISuhuSvKU3H8hm.EVaWgckR/9JLncCxyFa4kfM0yUOlIUS', 2, 'NO'),
+(9, 'Alvaro', 'Montoro', 'alvarobota123', 'alvaromontoro@botafogo.com', '$2y$10$v.Q1lRjPxMKyNh6RWeRa0eWEJivw9rpNP3YnsFZrN/R0ijRHKbJqS', 2, 'NO');
 
 --
 -- Índices para tablas volcadas
@@ -82,8 +84,7 @@ ALTER TABLE `perfiles`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `perfil_id` (`perfil_id`);
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -99,17 +100,7 @@ ALTER TABLE `perfiles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`perfil_id`) REFERENCES `perfiles` (`id_perfiles`);
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
